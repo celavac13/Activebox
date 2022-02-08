6efc30a0506820c2c4802e55c34ddbbe20c82525
Stefan Stevanovic <stefan.stevanovic@quantox.com>
2022-02-08 12:28:13 +0100

    Added carousel to testimonials section

--- Activebox/resuorces/js/index.js
+++ Activebox/resuorces/js/index.js
@@ -3,6 +3,13 @@ let navBar = document.getElementById("js--nav");
 let toggleIon = document.getElementsByClassName("js--ion")[0];
 let hamburgerBox = document.getElementsByClassName("hamburger-menu")[0];
 
+let commentHolder = document.getElementsByClassName("js--comment")[0];
+let nameHolder = document.getElementsByClassName("js--name")[0];
+let dots = document.getElementsByClassName("dot");
+let dotsHolder = document.getElementsByClassName("dot-box")[0];
+let indexSlider = 0;
+
+// mobile navigation menu
 toggleBtn.addEventListener("click" , () => {
     if (navBar.classList.contains("active")) {
         navBar.classList.remove("active");
@@ -14,3 +21,27 @@ toggleBtn.addEventListener("click" , () => {
         hamburgerBox.classList.add("active");
     }
 })
+
+
+//carousel
+
+dotsHolder.addEventListener("click", () => {
+    fetch("resuorces/js/coments.json")
+    .then(response => response.json())
+    .then(data => carouselComments(data, indexSlider));
+});
+
+for (let i = 0; i < dots.length; i++) {
+    dots[i].addEventListener("click", function() {
+        indexSlider = i;
+        for (let j = 0; j < dots.length; j++) {
+            dots[j].classList.remove("active");
+        }
+        dots[i].classList.add("active");
+    });
+};
+
+let carouselComments = (comm, i) => {
+    commentHolder.textContent = comm[i].comment;
+    nameHolder.textContent = comm[i].name;
+}
